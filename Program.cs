2cf4e2477a239b794e4a477845466ab5cf0688fc
PikeCaptain <idunno6418@gmail.com>
2024-04-30 13:16:04 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Askes user to input a character and assigns input to a variable
+            Console.WriteLine("Please input a character!");
+            char myChar = Console.ReadKey().KeyChar;
+
+            // Checks is th character user entered is a letter
+            bool isLetter = Char.IsLetter(myChar);
+
+            // Prints the results to console
+            Console.WriteLine($"\n{myChar} is a letter: {isLetter}.");
+
+            // Asks user a question and, saves response to a variable, sentence
+            Console.WriteLine("Why do you like video games?");
+            string sentence = Console.ReadLine();
+
+            // Asks user a question and, saves response to a variable called word
+            Console.WriteLine("Enter a word");
+            string word = Console.ReadLine();
+
+            // Checks is the word is in the sentence
+            bool isIn = sentence.Contains(word);
+
+            // Prints if the word is in the sentence
+            Console.WriteLine($"{word} exists in {sentence}, {isIn}!");
+        }
+    }
+}
